bb1ec732d5615a8f57a7e35e6b32aec7c3fd9a5c
Dmitry Vashlaev <vashlaevdev@gmail.com>
2019-11-28 21:25:44 +0200

    #Initial commit

---
+++ Filters/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Filters
+//
+//  Created by Дмитрий Вашлаев on 28/11/2019.
+//  Copyright © 2019 Dmitry Vashlaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
